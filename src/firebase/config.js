10374edb5a4db68e5d2691215c184f022cfbe126
Jagrat Kharbanda <kharbanda.jagrat25@gmail.com>
2021-07-01 17:49:16 +0530

    'hiding-keys'

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBqzMokRPAtONu18Lji7OY1xYxzIJfclWg",
-  authDomain: "firegram-1f8d5.firebaseapp.com",
-  projectId: "firegram-1f8d5",
-  storageBucket: "firegram-1f8d5.appspot.com",
-  messagingSenderId: "659015757148",
-  appId: "1:659015757148:web:2705f3352b5524dad7e599"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
